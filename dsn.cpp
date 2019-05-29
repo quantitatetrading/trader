4ae1c8764be13604f303611b8ee472419d2cd5c1
BrandtWP <48268950+BrandtWP@users.noreply.github.com>
2019-05-29 15:59:21 -0700

    Added mssql connection

---
+++ dsn.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
+#include <stdio.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {  
+   SQLHENV henv;  
+   SQLHDBC hdbc;  
+   SQLHSTMT hstmt;  
+   SQLRETURN retcode;  
+  
+   SQLCHAR * OutConnStr = (SQLCHAR * )malloc(255);  
+   SQLSMALLINT * OutConnStrLen = (SQLSMALLINT *)malloc(255);  
+  
+   // Allocate environment handle  
+   retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);  
+  
+   // Set the ODBC version environment attribute  
+   if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {  
+      retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void*)SQL_OV_ODBC3, 0);   
+  
+      // Allocate connection handle  
+      if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {  
+         retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);   
+  
+         // Set login timeout to 5 seconds  
+         if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {  
+            SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);  
+  
+            // Connect to data source  
+            retcode = SQLConnect(hdbc, (SQLCHAR*) "mssql", SQL_NTS, (SQLCHAR*) "QuantitateAdmin", 15, "SomeRandomPassword1", 19);  
+  
+            // Allocate statement handle  
+            if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {  
+
+                cout << "Success!" << endl;
+
+               retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);   
+  
+               // Process data  
+               if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {  
+                  SQLFreeHandle(SQL_HANDLE_STMT, hstmt);  
+               }  
+  
+               SQLDisconnect(hdbc);  
+            } else {
+                cout << "Error" << endl;
+            }  
+  
+            SQLFreeHandle(SQL_HANDLE_DBC, hdbc);  
+         }  
+      }  
+      SQLFreeHandle(SQL_HANDLE_ENV, henv);  
+   }  
+}  
